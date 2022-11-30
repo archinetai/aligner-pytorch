68d57e17688f27730f849e4d244f32070b7d4867
Flavio Schneider <flavio.schneider.97@gmail.com>
2022-11-30 08:18:25 +0100

    fix: imports

--- aligner_pytorch/__init__.py
+++ aligner_pytorch/__init__.py
@@ -1,2 +1,7 @@
-from aligner_pytorch.mas import mas
-from .aligner import *  # noqa
+from .mas import mas
+from .aligner import (
+    get_alignment_from_embeddings,
+    get_sequential_masks,
+    get_alignment_from_duration,
+    get_alignment_from_duration_embedding,
+)
